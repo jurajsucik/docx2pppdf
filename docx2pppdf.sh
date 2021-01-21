2f5771b7cf61b6fbf670b6e82ca275a666029556
Juraj Sucik <juraj.sucik@mandiant.com>
2021-01-21 18:29:42 +0100

    added error check

--- docx2pppdf.sh
+++ docx2pppdf.sh
@@ -9,6 +9,14 @@ echo "converting $1 to PDF"
 pdf=$1.pdf
 docx2pdf "$1" "$pdf"
 
-pppdf=$(echo $1 | sed 's/.docx/.pdf/')
-echo "adding password protection to $pppdf"
-pdftk "$pdf" output "$pppdf" userpw $2
+if [ $? -eq 0 ]
+then
+  echo "conversion to PDF was successful"
+  ls -l "$pdf"
+  pppdf=$(echo $1 | sed 's/.docx/.pdf/')
+  echo "adding password protection to $pppdf"
+  pdftk "$pdf" output "$pppdf" userpw $2
+else
+  echo "conversion to PDF was not successful" >&2
+  exit 1
+fi
